05da50e33208ce945e43d0c6918f9ffeb33918b3
Julien Poissonnier <julien@caffeine.lu>
2025-02-10 17:20:52 +0100

    Initial commit

---
+++ my-component.py
@@ -0,0 +1,25 @@
+import pulumi_random as random
+from typing import Optional, TypedDict
+
+import pulumi
+
+class MyComponentArgs(TypedDict):
+    who: Optional[pulumi.Output[str]]
+
+class MyComponent(pulumi.ComponentResource):
+    greeting: Optional[pulumi.Output[str]]
+
+    def __init__(self, name: str, args: MyComponentArgs, opts: Optional[pulumi.ResourceOptions] = None):
+        super().__init__("my-provider:index:MyComponent", name, {}, opts)
+        who = args.get("who") or "Pulumipus"
+        greeting_word = random.RandomShuffle(
+            f"{name}-greeting",
+            inputs=["Hello", "Bonjour", "Ciao", "Hola"],
+            result_count=1,
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+        self.greeting = pulumi.Output.concat(greeting_word.results[0], who, "!")
+        self.register_outputs({
+            "greeting": self.greeting,
+        })
+
